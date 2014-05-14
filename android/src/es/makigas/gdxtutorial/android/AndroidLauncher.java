e998aa59a929c1551d510d09d5798b8b348a07bd
Dani Rodríguez <danirod@outlook.com>
2014-05-14 19:07:59 +0200

    Commit inicial

---
+++ android/src/es/makigas/gdxtutorial/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package es.makigas.gdxtutorial.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import es.makigas.gdxtutorial.HolaGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new HolaGdxGame(), config);
+	}
+}
---
+++ desktop/src/es/makigas/gdxtutorial/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package es.makigas.gdxtutorial.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import es.makigas.gdxtutorial.HolaGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HolaGdxGame(), config);
+	}
+}
